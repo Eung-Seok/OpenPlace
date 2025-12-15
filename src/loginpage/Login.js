a2230240eaeaba9e874e7d5531399a74079f4ea5
Eung-Seok <rwjflrk1234@gmail.com>
2025-12-15 16:23:21 +0900

    login 비동기화 개선

--- src/loginpage/Login.js
+++ src/loginpage/Login.js
@@ -15,10 +15,15 @@ function Login() {
     let [modalFlag2,setModalFlag2] = useState(false);
     let [modalFlag3,setModalFlag3] = useState(false);
     let navigate = useNavigate();
-    let [isValid, setIsValid] = useState(true);
+    let [isValid, setIsValid] = useState(false);
+    let [isButtonOff, setIsButtonOff] = useState(true);
     let accountList = JSON.parse(localStorage.getItem('계정목록'))
     let [accountInfo,setAccountInfo] = useState(JSON.parse(localStorage.getItem('계정정보')))
     let [index, setIndex] = useState(0);
+
+    useEffect(()=>{
+        setIsButtonOff(idinput.trim() == '' || pwinput.trim() == '')
+    }, [idinput, pwinput])
 
     return (
         <div>
@@ -28,35 +33,30 @@ function Login() {
                     accountList.map((item, index)=>{
                         if(idinput == item.id && pwinput == item.pw){
                             setIndex(index)
-                        }else{
-                            setIsValid(false)
+                            setIsValid(true);
                         }
                     })
                     if(idinput.trim() == ''){
                         setModalFlag1(true);
                         setModalFlag2(false);
                         setModalFlag3(false);
-                    }else if(pwinput.trim() == ''){
+                        return;
+                    }
+                    if(pwinput.trim() == ''){
                         setModalFlag1(false);
                         setModalFlag2(true);
                         setModalFlag3(false);
-                    } else if(!isValid){
+                        return;
+                    }
+                    const isValid = accountList.find(item => item.id == idinput && item.pw == pwinput);
+                    if(isValid){
+                        localStorage.setItem('로그인현황', JSON.stringify(true))
+                        localStorage.setItem('계정정보', JSON.stringify(isValid))
+                        navigate('/community/main/1')
+                    } else{
                         setModalFlag1(false);
                         setModalFlag2(false);
                         setModalFlag3(true);
-                    } else{
-                        localStorage.setItem('로그인현황', JSON.stringify(true))
-                        let temp  = {
-                            nickname: accountList[index].nickname,
-                            name: accountList[index].name,
-                            mailAdress: accountList[index].mailAdress,
-                            phoneNumber: accountList[index].phoneNumber,
-                            birthday: accountList[index].birthday,
-                            id: accountList[index].id,
-                            pw: accountList[index].pw
-                        }
-                        localStorage.setItem('계정정보', JSON.stringify(temp))
-                        navigate('/community/main/1')
                     }
                 }}>
                     <div className='login-input-container'>
@@ -69,29 +69,17 @@ function Login() {
                                 loginSubmitButton.focus();
                             }} onChange={(event) => {
                                 setIdInput(event.target.value);
-                                if (idinput.trim() != '' && pwinput.trim() != '') {
-                                    loginSubmitButton.classList.remove('off')
-                                } else {
-                                    loginSubmitButton.classList.add('off')
-                                }
                             }} />
                             <label for='id' className='login-text-label' aria-hidden='true'>아이디 또는 전화번호</label>
                         </div>
                         <div className={'login-pw-input-container ' + (pwFocus1 ? 'focus1 ' : ' ') + (pwFocus2 ? 'focus2' : ' ')}>
-                            <input type='text' value={pwinput} id='pw' name='pw' className='login-pw-input' onFocus={() => { setPwFocus1(true); setPwFocus2(true) }} onBlur={() => {
+                            <input type='password' value={pwinput} id='pw' name='pw' className='login-pw-input' onFocus={() => { setPwFocus1(true); setPwFocus2(true) }} onBlur={() => {
                                 setPwFocus1(false)
                                 if (pwinput.trim() == '') {
                                     setPwFocus2(false)
                                 }
                                 loginSubmitButton.focus();
-                            }} onChange={(event) => {
-                                setPwInput(event.target.value);
-                                if (idinput.trim() != '' && pwinput.trim() != '') {
-                                    loginSubmitButton.classList.remove('off')
-                                } else {
-                                    loginSubmitButton.classList.add('off')
-                                }
-                            }} />
+                            }} onChange={(event) => {setPwInput(event.target.value);}} />
                             <label for="pw" className='login-text-label' aria-hidden='true'>비밀번호</label>
                         </div>
                     </div>
@@ -99,7 +87,7 @@ function Login() {
                         {modalFlag2 && <div style={{color:'red', fontSize:'14px', marginTop:'20px'}}>비밀번호를 입력해주세요</div>}
                         {modalFlag3 && <div style={{color:'red', fontSize:'14px', marginTop:'20px'}}>아이디 또는 비밀번호가 잘못되었습니다. 아이디와 비밀번호를 정확히 입력해주세요</div>}
                     <div>
-                        <button type='submit' id='login-submit-button' className='login-submit-button off'>
+                        <button type='submit' id='login-submit-button' className={'login-submit-button '+ (isButtonOff ? 'off' : ' ')}>
                             <span className='login-button-text'>로그인</span>
                         </button>
                     </div>
